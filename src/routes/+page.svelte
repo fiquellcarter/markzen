ba0941befb7bde06f34eda7fae4eb91089d5b26b
Fiquell Carter <fiquellcarter@gmail.com>
2026-01-12 01:45:14 +0800

    feat(ui): setup shadcn-svelte and add button component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<section>
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+	<Button>Check</Button>
+</section>
