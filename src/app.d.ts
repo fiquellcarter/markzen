6b569ee4d83154c9efabbd2685f5be106974ad44
Fiquell Carter <fiquellcarter@gmail.com>
2026-01-12 08:42:02 +0800

    feat(auth): add session and user to event locals

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,10 @@
 declare global {
   namespace App {
+    interface Locals {
+      session: import('better-auth').Session;
+      user: import('better-auth').User;
+    }
+
     // interface Error {}
     // interface Locals {}
     // interface PageData {}
