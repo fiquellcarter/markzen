503bec0a55ac82ab9697796903323c1e7bd92829
Fiquell Carter <fiquellcarter@gmail.com>
2026-01-21 01:54:30 +0800

    refactor(collection): simplify session handling in load function

--- src/routes/(app)/collection/[slug]/+page.server.ts
+++ src/routes/(app)/collection/[slug]/+page.server.ts
@@ -1,26 +1,21 @@
-import { error, redirect } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import { and, eq } from 'drizzle-orm';
 import { superValidate } from 'sveltekit-superforms';
 import { zod4 } from 'sveltekit-superforms/adapters';
 
-import { auth } from '$lib/auth/server';
 import { deleteCollectionSchema, updateCollectionSchema } from '$lib/schemas/collection';
 import { db } from '$lib/server/db';
 import { collection } from '$lib/server/db/schema';
 
-export async function load({ params, request }) {
-  const session = await auth.api.getSession({
-    headers: request.headers,
-  });
-
-  if (!session) {
-    return redirect(302, '/sign-in');
-  }
+export async function load({ params, parent }) {
+  const parentData = await parent();
 
   const [existingCollection] = await db
     .select()
     .from(collection)
-    .where(and(eq(collection.userId, session.user.id), eq(collection.slug, params.slug)));
+    .where(
+      and(eq(collection.userId, parentData.session.user.id), eq(collection.slug, params.slug))
+    );
 
   if (!existingCollection) {
     return error(404, 'Collection not found');
